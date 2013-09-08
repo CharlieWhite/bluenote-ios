996e7c3c1d2b76a2c1550520703577414cb10c80
Charlie White <charlie@anchor.io>
2013-09-07 17:51:25 -0700

    Initial Commit

---
+++ bluenote-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bluenote-ios
+//
+//  Created by Charlie White on 9/7/13.
+//  Copyright (c) 2013 Charlie White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
