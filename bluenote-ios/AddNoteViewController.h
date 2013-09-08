86e2ba214d51423904ce00c26e368ebb1268f700
Charlie White <charlie@anchor.io>
2013-09-08 02:35:03 -0700

    add note screen

---
+++ bluenote-ios/AddNoteViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddNoteViewController.h
+//  bluenote-ios
+//
+//  Created by Charlie White on 9/8/13.
+//  Copyright (c) 2013 Charlie White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNoteViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *noteTextField;
+
+@end
---
+++ bluenote-ios/AddNoteViewController.m
@@ -0,0 +1,55 @@
+//
+//  AddNoteViewController.m
+//  bluenote-ios
+//
+//  Created by Charlie White on 9/8/13.
+//  Copyright (c) 2013 Charlie White. All rights reserved.
+//
+
+#import "AddNoteViewController.h"
+#import "Note.h"
+
+@interface AddNoteViewController ()
+
+@end
+
+@implementation AddNoteViewController
+
+@synthesize noteTextField;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    
+    [super viewDidLoad];
+    UIBarButtonItem *noteButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(sendNote:)];
+    self.navigationItem.rightBarButtonItem = noteButton;
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)sendNote:(id)sender {
+    
+    Note *note = [[Note alloc] init];
+    note.message = noteTextField;
+    [[RKObjectManager sharedManager] postObject:note path:@"notes" parameters:nil success:nil failure:nil];
+    
+
+    
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
